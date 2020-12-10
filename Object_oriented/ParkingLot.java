ca5a39a130fb172aee5c19717f1059d1ac18972f
swagatCB <swagat.patra@careerbuilder.com>
2020-12-10 12:20:38 +0530

    system design and object oriented design

---
+++ Object_oriented/ParkingLot.java
@@ -0,0 +1,20 @@
+package System_Design.Object_oriented;
+
+// singleton
+public class ParkingLot {
+    
+}
+
+class Ticket{
+
+}
+
+class ParkingSpot{
+    enum size{
+        small, medium, large
+    };
+    int row; int col; int floor;
+}
+
+// class 
+
